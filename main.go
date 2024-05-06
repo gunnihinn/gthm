5f699f59adac7f50d426d2229538fe6414d016de
Gunnar Þór Magnússon <gunnarm@tern.is>
2024-05-06 10:15:06 +0000

    Use template

--- main.go
+++ main.go
@@ -1,26 +1,55 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("postHandler: url=%s", r.URL)
-	blob, err := os.ReadFile("index.html")
+type blog struct {
+	index *template.Template
+}
+
+func newBlog(filename string) (blog, error) {
+	blob, err := os.ReadFile(filename)
 	if err != nil {
-		log.Printf("error: Couldn't read HTML: %s", err)
+		return blog{}, fmt.Errorf("error: Couldn't read %s: %s", filename, err)
+	}
+	tmpl, err := template.New("index").Parse(string(blob))
+	if err != nil {
+		return blog{}, fmt.Errorf("error: Couldn't parse %s template: %s", filename, err)
+	}
+
+	return blog{index: tmpl}, nil
+}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("postHandler: url=%s", r.URL)
+
+	var buf bytes.Buffer
+	if err := b.index.Execute(&buf, nil); err != nil {
+		log.Printf("error: Couldn't generate HTML: %s", err)
 		http.Error(w, fmt.Sprintf("Couldn't serve HTML"), http.StatusInternalServerError)
 		return
 	}
 
-	w.Write(blob)
+	if _, err := buf.WriteTo(w); err != nil {
+		log.Printf("error: Couldn't write HTML: %s", err)
+		http.Error(w, fmt.Sprintf("Couldn't serve HTML"), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-	http.HandleFunc("/", postHandler)
+	blog, err := newBlog("index.html")
+	if err != nil {
+		log.Fatalf("error: Couldn't create blog: %s", err)
+	}
+
+	http.Handle("/", blog)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
